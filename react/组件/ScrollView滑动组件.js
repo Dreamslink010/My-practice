9297ff88bd1bc831910a4178162fcf0299fcdc79
Dreamslink010 <624848223@qq.com>
2017-08-10 17:29:01 +0800

    滑动组件

---
+++ react/组件/ScrollView滑动组件.js
@@ -0,0 +1,27 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+
+	import React, { Component } from 'react';
+	import {
+	  AppRegistry,
+	  StyleSheet,
+	  Text,
+	  View
+	} from 'react-native';
+
+	//练习1，实现ScrollView基本功能
+	
+	var HelloWorld = require("./myScrollView");
+	
+	
+	
+	//练习2，电影列表
+	
+	
+	
+	
+	AppRegistry.registerComponent('HelloWorld', () => HelloWorld);
